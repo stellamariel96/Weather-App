a357ccc6cebdf9dc4885f37d390d7d288ba94411
stellamariel96 <stella-marie.landt@hotmail.com>
2021-03-16 11:03:05 +0100

    Unit and Forecast

--- java.js
+++ java.js
@@ -14,6 +14,19 @@ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 
 currentDate.innerHTML = `${days[day]} ${hours}:${minutes}`;
 
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+    return `${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
     console.log(response.data);
     document.querySelector("#city").innerHTML = response.data.name;
@@ -23,12 +36,37 @@ function displayWeather(response) {
     document.querySelector("#wind").innerHTML = response.data.wind.speed;
     document.querySelector("#condition").innerHTML = response.data.weather[0].main;
     document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+    celsiusTemperature = response.data.main.temp;
+}
+
+function displayForecast(response) {
+    let forecastElement = document.querySelector("#forecast");
+    let forecast = response.data.list[0];
+    console.log(forecast);
+
+    forecastElement.innerHTML = `
+    <div class="col-sm-4">
+    ${formatHours(forecast.dt * 1000)}
+        </div>
+    <div class="col-sm-4">
+        <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" id="icons">
+    </div>
+    <div class="col-sm-4">
+        <div class="temp">
+            <strong>${Math.round(forecast.main.temp_max)}°C</strong> | ${Math.round(forecast.main.temp_min)}°C
+        </div>
+    </div>
+    `;
 }
 
 function searchCity(city) {
 let apiKey = "5906ab66e299beda90276c8e66401132";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
      axios.get(apiUrl).then(displayWeather);
+
+     apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+     axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -50,11 +88,36 @@ function getCurrentLocation(event) {
     navigator.geolocation.getCurrentPosition(searchLocation)
     }
 
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature *9/5)+32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
 let currentCity = document.querySelector("#city");
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-location");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Vienna");
